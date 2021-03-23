250199fa0c8f899adc9d95450f9b6ee1e157751a
AtlasC0R3 <39468657+AtlasC0R3@users.noreply.github.com>
2021-03-23 08:48:15 -0400

    Added comments and stuff

--- drop/__main__.py
+++ drop/__main__.py
@@ -3,15 +3,16 @@ import os
 
 run_gui = True
 
-if (os.getenv("DISPLAY") is None) and os.name == 'posix':
+# check if it'll be possible to run drop-gui
+if (os.getenv("DISPLAY") is None) and (os.name == 'posix'):
     run_gui = "Cannot run drop-gui due to no DISPLAY variable: this likely means that this session is running " \
               "without graphics interface, such as in a tty terminal."
+# i have no idea how this will work for Windows. can you even not run have a UI on Windows? is that even possible?
 try:
     from dearpygui.core import *
     from dearpygui.simple import *
 except ImportError:
     run_gui = "Dear PyGui not installed, no way of running the GUI without the core GUI library."
-# check if it'll be possible to run drop-gui
 
 if type(run_gui) is not str:
     import drop.gui

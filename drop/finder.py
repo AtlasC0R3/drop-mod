e6415aeaabaac5a69281abfc6e7d821e667bba45
AtlasC0R3 <39468657+AtlasC0R3@users.noreply.github.com>
2021-03-22 22:26:33 -0400

    Added a function to find all warned users inside a guild

--- drop/finder.py
+++ drop/finder.py
@@ -18,3 +18,15 @@ def find_todos():
         if file.endswith(".json"):
             todo_files.append(file.split('.')[0])
     return todo_files
+
+
+def find_warns(guild_id: int):
+    warn_files = []
+    try:
+        files = os.listdir(f"data/servers/{guild_id}/warns/")
+    except FileNotFoundError:
+        return warn_files
+    for file in files:
+        if file.endswith(".json"):
+            warn_files.append(file.split('.')[0])
+    return warn_files

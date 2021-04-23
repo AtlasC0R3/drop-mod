aed68a9b14ea86c24ddc974c9ef25e63659320f9
AtlasC0R3 <39468657+AtlasC0R3@users.noreply.github.com>
2021-04-23 08:29:39 -0400

    Added a batch file to install drop-mod on Windows

---
+++ install.bat
@@ -0,0 +1,14 @@
+@echo off
+:: extremely basic because i dont know how to batch.
+
+python --version 3>NUL
+if errorlevel 1 goto errorNoPython
+
+py -m pip uninstall drop-mod -y
+py setup.py install
+
+goto:eof
+
+:errorNoPython
+echo.
+echo Error^: Python not installed

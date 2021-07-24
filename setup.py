f3ad803de34cda3faf44c6e839f04282ae02b126
AtlasC0R3 <39468657+AtlasC0R3@users.noreply.github.com>
2021-07-24 14:08:08 -0400

    updated setup.py a bit

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ from drop import __version__
 with open('requirements.txt', 'r', encoding='utf-8') as f:
     install_requires = [line for line in f.read().splitlines() if len(line) > 0]
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(name='drop-mod',
       version=__version__,
       description='A Python moderation toolkit built for chat bots',
+      long_description=readme,
+      long_description_content_type="text/markdown",
       url='https://github.com/AtlasC0R3/drop-moderation',
       author='atlas_core',
       license='Apache 2.0',
